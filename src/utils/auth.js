a8ab186bdd42c5d385b76f4e4e6548c2f81f2119
VDRama72 <vdrama72@gmail.com>
2025-07-10 06:26:25 +0800

    Fix responsive layout for Admin Panel

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,5 +1,4 @@
 // src/utils/auth.js
-
 export function logout() {
   const role = localStorage.getItem('role');
 
@@ -8,14 +7,11 @@ export function logout() {
   localStorage.removeItem('role');
   localStorage.removeItem('userId');
   localStorage.removeItem('userName');
+  localStorage.removeItem('disclaimerAccepted');
 
   // 🔁 Paksa sinkronisasi ke komponen yang memakai localStorage
   window.dispatchEvent(new Event('storage'));
 
-  // 🔁 Redirect
-  if (role === 'penjual') {
-    window.location.href = '/login'; // misalnya nanti ada login seller
-  } else {
-    window.location.href = '/login'; // bisa juga '/' tergantung kebutuhan
-  }
+  // ✅ Redirect langsung ke Etalase
+  window.location.href = '/';
 }
